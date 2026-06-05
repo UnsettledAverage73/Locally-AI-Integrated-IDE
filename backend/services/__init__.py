fa61609bd9e8e46f65b50408eef7f932450f2629
unsettledaverage73 <atharvabodade@gmail.com>
2026-06-05 18:31:42 +0530

    chore: export new AI-native services

--- backend/services/__init__.py
+++ backend/services/__init__.py
@@ -1,3 +1,5 @@
 from .OllamaService import OllamaService
 from .RAGService import RAGService
 from .MemoryService import MemoryService
+from .CodebaseMapService import codebase_map_service
+from .FlowService import flow_service
